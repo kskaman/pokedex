cd58e2afa67b8a1b2559db1de034b9151d95ec92
root <root@Kaman.>
2025-06-11 09:55:00 -0230

    started pokedex by boo.dev, done part 3 chapter 1

---
+++ src/repl.test.ts
@@ -0,0 +1,35 @@
+import { cleanInput } from './repl';
+
+import { describe, test, expect } from 'vitest';
+
+describe.each([
+    {
+        input: " hello world ",
+        expected: ["hello", "world"],
+    },
+    {
+        input: "   test   string   ",
+        expected: ["test", "string"],
+    },
+    {
+        input: "  spaced   out  ",
+        expected: ["spaced", "out"],
+    },
+    {
+        input: "one",
+        expected: ["one"],
+    },
+    {
+        input: "",
+        expected: [],
+    },
+])("cleanInput($input)", ({ input, expected }) => {
+    test(`Expected: ${expected}`, () => {
+        const actual = cleanInput(input); // ✅ Call the function with the input
+
+        expect(actual).toHaveLength(expected.length);
+        for (const i in expected) {
+            expect(actual[i]).toBe(expected[i]);
+        }
+    });
+});
