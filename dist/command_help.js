37f7e35f51dde97b6c25ec10a4d9a276c928ece2
root <root@Kaman.>
2025-06-13 11:45:11 -0230

    complete chapter 2 cahce  lecture 2

--- dist/command_help.js
+++
@@ -1,7 +0,0 @@
-export async function commandHelp(state) {
-    console.log("Welcome to the Pokedex!\nUsage:\n");
-    for (const command of Object.values(state.commands)) {
-        console.log(`${command.name}: ${command.description}`);
-    }
-    console.log();
-}
